049ff15a239d8cad584c88d2981b1a9d70fc3653
Lilli Nappi <116825146+nappilil@users.noreply.github.com>
2024-04-23 23:40:53 -0400

    completed getAllAnnouncements function
    this way announcements will only show pertaining to the household you are in

--- data/announcementPost.js
+++ data/announcementPost.js
@@ -1,24 +1,33 @@
 import {announcements} from '../config/mongoCollections.js'; // import collection
 import {ObjectId} from 'mongodb';
+import {checkId, checkString} from '../validation.js'
 
 const exportedMethods = {
-    async newAnnouncement (
-    action,
-    groceryItem,
-    groceryList,
-    comment,
-    userId
-  ) {
-    return;
-  },
+  // might get rid of this function because we can just make a add the new announcement within the functions like in joinHousehold
+  //   async newAnnouncement (
+  //   action,
+  //   comment,
+  //   userId
+  // ) {
+  //   return;
+  // },
 
   async deleteOldAnnouncement(id) {
     return;
   },
-  
-  async getAllAnnouncements() {
-    return;
+  // because if there are different households should only see announcements from your household
+  async getAllAnnouncementsByHouseholdName(householdName) {
+    householdName = checkString(householdName);
+    const announcementCollection = await announcements();
+    let announcementList = await announcementCollection
+    .find({householdName: householdName})
+    .project({ _id: 0, announcement: 1, comment: 1, currentDate: 1 })   // just return needed fields
+    .toArray();
+    if (!announcementList) throw `Could not get all Announcements`
+    announcementList.reverse(); // most recent announcements on top
+    return announcementList; // return list of announcements
   },
+
   async deleteComment(id) {
     return;
   },
