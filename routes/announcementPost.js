ca7aaa0a3e750cc97be9e32ced05fbc1def3ad1a
Lilli Nappi <116825146+nappilil@users.noreply.github.com>
2024-04-23 23:44:39 -0400

    render announcement tab

--- routes/announcementPost.js
+++ routes/announcementPost.js
@@ -2,9 +2,16 @@ import {Router} from 'express';
 const router = Router();
 import {announcementData} from '../data/index.js';
 
-
 router.route('/')
   .get(async (req, res) => {
+    const user = req.session.user;
+    const announcements = await announcementData.getAllAnnouncementsByHouseholdName(user.householdName);
+    res.render('announcements', 
+    {pageTitle: "Announcements Tab",
+    authenticated: true,
+    household: true,
+    announcements
+  })
 })
  .post(async (req,res) => {
 
