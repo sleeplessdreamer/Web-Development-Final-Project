92368d013530ab3d6109b514fe0f91351e95fe95
Lilli Nappi <116825146+nappilil@users.noreply.github.com>
2024-04-23 23:44:02 -0400

    created joinAnnouncement() + fixed joinHousehold()
    somehow forgot to update user information to include the householdName they just joined. Also created announcement for joinAnnouncement just to see how it can work for other functions

--- data/household.js
+++ data/household.js
@@ -1,26 +1,26 @@
-import {checkId, checkString} from '../validation.js'
-import {household, users} from '../config/mongoCollections.js'; // import collection
+import { checkId, checkString } from '../validation.js'
+import { household, users, announcements } from '../config/mongoCollections.js'; // import collection
 import userData from './users.js'
-import {ObjectId} from 'mongodb';
+import { ObjectId } from 'mongodb';
 
 const exportedMethods = {
-    async createHousehold (householdName, userId) {
+  async createHousehold(householdName, userId) {
     // Error Checking
     householdName = checkString(householdName, "Household Name")
     userId = checkId(userId, "User ID")
-    
+
     // Check if Household name already exists
     const householdCollection = await household();
-    const existingHousehold = await householdCollection.find({householdName: householdName}).toArray();
+    const existingHousehold = await householdCollection.find({ householdName: householdName }).toArray();
     if (existingHousehold.length !== 0) throw `Error: that Household Name already exists`;
-    
+
     // Get name of user who created household and put them in members list
     const userMember = await userData.getUserById(userId);
     if (!userMember) throw `User could not be found`;
-    
+
     // Create New Household
     let members = [userMember.firstName + " " + userMember.lastName], // put the user who created the id in the household
-    groceryLists = [] 
+      groceryLists = []
     const newhouseHold = {
       householdName,
       members,
@@ -38,9 +38,9 @@ const exportedMethods = {
     }
     const userCollection = await users();
     const updatedInfo = await userCollection.findOneAndUpdate(
-      {_id: new ObjectId(userId)},
-      {$set: updatedUser},
-      {returnDocument: 'after'}
+      { _id: new ObjectId(userId) },
+      { $set: updatedUser },
+      { returnDocument: 'after' }
     );
     // if user cannot be updated method should throw
     if (!updatedInfo) {
@@ -49,32 +49,101 @@ const exportedMethods = {
     newhouseHold._id = newhouseHold._id.toString(); // convert to string
     return newhouseHold;
   },
-  
+
   async joinHousehold(householdName, userId) {
     householdName = checkString(householdName, "Household Name");
     userId = checkId(userId, "User Id");
 
     const householdCollection = await household();
-    const existingHousehold = await householdCollection.find({householdName: householdName}).toArray();
+    const existingHousehold = await householdCollection.find({ householdName: householdName }).toArray();
     if (existingHousehold.length === 0) throw `Error: Household does not exist`;
+
     // Get name of user who created is joining household and put them in members list
     const userMember = await userData.getUserById(userId);
     if (!userMember) throw `User could not be found`;
     let updatedInfo = await householdCollection.findOneAndUpdate(
-      {_id: new ObjectId(existingHousehold[0]._id)},
-      {$push: {members: userMember.firstName + " " + userMember.lastName}},
-      {returnDocument: "after"}
+      { _id: new ObjectId(existingHousehold[0]._id) },
+      { $push: { members: userMember.firstName + " " + userMember.lastName } },
+      { returnDocument: "after" }
     );
     if (!updatedInfo) {
       throw 'Error: Could not update household successfully';
     }
     updatedInfo._id = updatedInfo._id.toString();
-    return updatedInfo;
+    // Add householdName to user info
+    const updatedUser = {
+      householdName: householdName,
+    }
+    // Get user and add householdName they just joined to their information
+    const userCollection = await users();
+    updatedInfo = await userCollection.findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      { $set: updatedUser },
+      { returnDocument: 'after' }
+    );
+    // if user cannot be updated method should throw
+    if (!updatedInfo) {
+      throw 'Error: Could not update user successfully';
+    }
+    await this.joinAnnouncement(householdName, userId); // make announcement
+    return updatedInfo; // return household with new member
   },
+  async joinAnnouncement(householdName, userId) {
+    householdName = checkString(householdName, "Household Name");
+    userId = checkId(userId, "User Id");
+
+    // Get name of user who is joining household
+    const userMember = await userData.getUserById(userId);
+    if (!userMember) throw `User could not be found`;
+
+    // Make New Announcement
+    const announcement = userMember.firstName + " " + userMember.lastName + " joined the household!"
+    let currentDate = new Date(), groceryList = [], groceryItem = "", action = "join", comment = "";
+    let day = currentDate.getUTCDate()
+    let month = currentDate.getUTCMonth() + 1
+    const year = currentDate.getUTCFullYear();
+    if (month.toString().length === 1) {
+      month = "0" + month;
+    }
+    if (day.toString().length === 1) {
+      day = "0" + day;
+    }
+    currentDate = month + "/" + day + "/" + year;
+
+    const newAnnouncement = {
+      action, // might get rid of this 
+      groceryList,
+      groceryItem,
+      comment,
+      userId,
+      householdName,
+      announcement, // added this for text to store in database
+      currentDate
+    }
+    // Insert New Announcement
+    const announcementCollection = await announcements();
+    const insertInfo = await announcementCollection.insertOne(newAnnouncement);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) throw 'Error: Could not add announcement';
+
+    // Update user information with new announcement
+    const userCollection = await users();
+    let updatedInfo = await userCollection.findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      { $push: { announcements: newAnnouncement } },
+      { returnDocument: "after" }
+    );
+    if (!updatedInfo) {
+      throw 'Error: Could not update user successfully';
+    }
+    updatedInfo._id = updatedInfo._id.toString();
+
+    return insertInfo;
+  },
+
   async getHouseholdByName(householdName) {
     householdName = checkString(householdName, 'Household Name');
     const householdCollection = await household();
-    const house = await householdCollection.find({householdName: householdName}).toArray();
+    const house = await householdCollection.find({ householdName: householdName }).toArray();
     if (!house) throw `Error: Household not found`;
     return house[0];
   },
@@ -86,5 +155,5 @@ const exportedMethods = {
     return members;
   }
 
-  };
-  export default exportedMethods;
+};
+export default exportedMethods;
