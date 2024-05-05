701d2fa0b52c933672e180e444e6784609a36864
Lilli Nappi <116825146+nappilil@users.noreply.github.com>
2024-05-05 19:30:52 -0400

    new: edit grocerylist
    created ajax form and routes so users can edit the grocery lists they created from their user profile.
    
    Co-Authored-By: rvictori25 <127045693+rvictori25@users.noreply.github.com>

--- routes/groceryList.js
+++ routes/groceryList.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 const router = Router();
-import { groceryListData } from '../data/index.js';
+import { groceryItemsData, groceryListData } from '../data/index.js';
 import { checkHouseholdName, checkString, checkId } from '../validation.js';
 import xss from 'xss';
 
@@ -65,7 +65,7 @@ router.route('/new')//hasn't been tested
     }
   });
 
-router.route('/:id')//This part works
+router.route('/:id')
   .get(async (req, res) => {
     const user = req.session.user;
     const listId = req.params.id;
@@ -81,9 +81,92 @@ router.route('/:id')//This part works
         listId
       });
     } catch (e) {
-      console.error('Error fetching grocery list:', e);
+      //console.error('Error fetching grocery list:', e);
       res.status(500).render('error', { error: e });
     }
   });
+
+router.route('/edit/:id')
+  .get(async (req, res) => {
+    const user = req.session.user;
+    const listId = req.params.id;
+    let groceryList;
+    try {
+      groceryList = await groceryListData.getGroceryList(listId);
+    } catch (e) {
+      res.status(500).render('error', { error: e });
+    }
+    try {
+      res.status(200).render('groceryList/edit', {
+        pageTitle: 'Edit Grocery List',
+        user,
+        authenticated: true,
+        groceryList: groceryList,
+        listId: listId,
+        household: true
+      });
+    } catch (e) {
+      //console.error('Error displaying edit grocery list form:', e);
+      res.status(500).render('error', { error: e });
+    }
+  })
+  .post(async (req, res) => {
+    let editData = req.body;
+    let groceryName, listType;
+    let listId = req.params.id;
+    let errors = [];
+    try {
+      listId = checkId(listId, "List Id");
+    } catch (e) {
+      errors.push(e);
+    }
+    let oldGroceryList = await groceryListData.getGroceryList(listId);
+    if (editData.groceryName) {
+      groceryName = xss(editData.groceryName);
+      try {
+        groceryName = checkString(groceryName, "Grocery List Name");
+      } catch (e) {
+        errors.push(e);
+      }
+    } else {
+      groceryName = oldGroceryList.groceryName;
+    }
+    if (editData.listType) {
+      listType = xss(editData.listType)
+      try {
+        if (listType.trim().toLowerCase() !== 'community')
+          if (listType.trim().toLowerCase() !== 'special occasion')
+            if (listType.trim().toLowerCase() !== 'personal') {
+              throw 'Not a valid list type';
+            }
+      } catch (e) {
+        errors.push(e);
+      }
+    } else {
+      listType = oldGroceryList.listType;
+    }
+    if (errors.length > 0) {
+      res.status(400).render('groceryList/edit', {
+        pageTitle: "New Grocery List",
+        errors: errors,
+        hasErrors: true,
+        groceryList: newListData,
+        listId: listId,
+        authenticated: true,
+        household: true
+      });
+      return;
+    }
+    try {
+      // Call the method to create a new grocery list item
+      let editListInfo = await groceryListData.updateGroceryList(listId, groceryName, listType);
+      if (!editListInfo) throw `Error could not edit list`;
+      return res.redirect('/household/info');
+    } catch (error) {
+      // Handle errors appropriately, for example, render an error page
+      res.status(500).render('error', { error: error });
+    }
+  })
+
 
 export default router;
