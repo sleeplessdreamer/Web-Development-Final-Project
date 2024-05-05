3dca10a4f9126bc08d7e5a41161f61872d3286df
Lilli Nappi <116825146+nappilil@users.noreply.github.com>
2024-05-04 22:49:02 -0400

    putting aditis client side validation back

--- public/js/log_in.js
+++ public/js/log_in.js
@@ -1,57 +1,82 @@
 (function ($) {
-// client-side validation for logging in 
-const checkEmail = (email, varName) => {
-    if (!email) throw `Error: You must supply a ${varName}!`;
-    if (typeof email !== 'string') throw `Error: ${varName} must be a string!`;
-    email = email.trim();
-    if (email.length === 0)
-      throw `Error: ${varName} cannot be empty or just spaces`;
-    // https://www.npmjs.com/package/email-validator imported npm that checks if valid email address
-    /*
-    if (!EmailValidator.validate(email))
-      throw `Error: ${email} is an invalid ${varName}`;
-    */
-    return; 
-}
-
-const checkPasswordLogin = (password, varName) => {
-    // Check if password is at least 8 chars, and combo of upper & lower case char, at least 1 digit, at least 1 special char.
-    if (!password) throw `Error: must supply a ${varName}`;
-    if (typeof password !== 'string') throw `Error: ${varName} must be a string!`;
-    password = password.trim();
-    if (password.length === 0) throw `Error: must supply a ${varName}`;
-    return;
-}
-
-$('#login-form').submit(function (event) {
-    event.preventDefault();
-
-    let email = $('#login-email').val();
-    let password = $('#login-password').val();
-
-    try {
-        checkEmail(email, 'Email Address');
-        checkPasswordLogin(password, 'Password');
-
-        
-        $.ajax({
-            method: 'POST',
-            url: '/users/login',
-            contentType: 'application/json',
-            data: JSON.stringify({
-                email: email,
-                password: password
-            })
-        }).done(function (response) {
-            
-            window.location.href = '/users/profile';
-        }).fail(function (error) {
-            
-            $('#login-error').text("Log in failed: " + error.responseJSON.message);
-        });
-    } catch (error) {
-        
-        $('#login-error').text(error);
+    // client-side validation for logging in 
+    const checkEmail = (email, varName) => {
+        if (!email) throw `Error: You must supply a ${varName}!`;
+        if (typeof email !== 'string') throw `Error: ${varName} must be a string!`;
+        email = email.trim();
+        if (email.length === 0)
+            throw `Error: ${varName} cannot be empty or just spaces`;
+        // https://www.npmjs.com/package/email-validator imported npm that checks if valid email address
+        /*
+        if (!EmailValidator.validate(email))
+          throw `Error: ${email} is an invalid ${varName}`;
+        */
+        return;
     }
-});
+
+    const checkPasswordLogin = (password, varName) => {
+        // Check if password is at least 8 chars, and combo of upper & lower case char, at least 1 digit, at least 1 special char.
+        if (!password) throw `Error: must supply a ${varName}`;
+        if (typeof password !== 'string') throw `Error: ${varName} must be a string!`;
+        password = password.trim();
+        if (password.length === 0) throw `Error: must supply a ${varName}`;
+        return;
+    }
+
+    // evert listener
+    document.getElementById('login-form').addEventListener('submit', function (event) {
+        //let errordiv = document.getElementById('error_div'); 
+        // prevent submission 
+        let password = document.getElementById('password').value;
+        let email = document.getElementById('email').value;
+        let perror = document.getElementById('perror');
+        let eerror = document.getElementById('eerror');
+        try {
+            let result = checkEmail(email, "Email Address");
+            eerror.classList.add('hidden');
+        } catch (e) {
+            event.preventDefault();
+            eerror.textContent = e;
+        }
+        try {
+            let result = checkPasswordLogin(password, "Password");
+            perror.classList.add('hidden');
+        } catch (e) {
+            event.preventDefault();
+            perror.textContent = e;
+        }
+        //return; 
+    });
+
+    $('#login-form').submit(function (event) {
+        event.preventDefault();
+
+        let email = $('#login-email').val();
+        let password = $('#login-password').val();
+
+        try {
+            checkEmail(email, 'Email Address');
+            checkPasswordLogin(password, 'Password');
+
+
+            $.ajax({
+                method: 'POST',
+                url: '/users/login',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+            }).done(function (response) {
+
+                window.location.href = '/users/profile';
+            }).fail(function (error) {
+
+                $('#login-error').text("Log in failed: " + error.responseJSON.message);
+            });
+        } catch (error) {
+
+            $('#login-error').text(error);
+        }
+    });
 })(window.jQuery);
--- public/js/sign_up.js
+++ public/js/sign_up.js
@@ -1,5 +1,5 @@
 (function ($) {
-const checkEmail = (email, varName) => {
+  const checkEmail = (email, varName) => {
     if (!email) throw `Error: You must supply a ${varName}!`;
     if (typeof email !== 'string') throw `Error: ${varName} must be a string!`;
     email = email.trim();
@@ -10,9 +10,9 @@ const checkEmail = (email, varName) => {
     if (!EmailValidator.validate(email))
       throw `Error: ${email} is an invalid ${varName}`;
     */
-    return; 
+    return;
   }
-  
+
   const checkPasswordSignUp = (password, varName) => {
     // Check if password is at least 8 chars, and combo of upper & lower case char, at least 1 digit, at least 1 special char.
     if (!password) throw `Error: ${varName} must be at least 8 chars, has one upper and lower case letter, 1 digit, & contains at least one special character`;
@@ -42,10 +42,10 @@ const checkEmail = (email, varName) => {
     if (!name.match(regEx)) throw `Error: invalid ${varName}`;
     return;
   }
-  
+
   // check age
   const checkAge = (age, varName) => {
-    if (!age) throw `Error: You must supply an ${varName}!`;  
+    if (!age) throw `Error: You must supply an ${varName}!`;
     if (typeof age !== 'number') {
       throw `${varName || 'provided variable'} is not a number`;
     }
@@ -57,7 +57,100 @@ const checkEmail = (email, varName) => {
     }
     return;
   }
-  
+
+  // then do a try catch in which errors are appended to error div
+
+  // at the very end add multiple divs that are under each input field 
+
+  document.getElementById('signup').addEventListener('submit', function (event) {
+    let errordiv = document.getElementById('error_div');
+    //errordiv.textContent = "heyhey"; 
+    // errordiv.classList.add()
+
+
+    //error.classList.add('hidden');
+    //$('.error').hide();
+    let firstname = document.getElementById('firstName').value;
+    let lastname = document.getElementById('lastName').value;
+    let age = document.getElementById('age').value;
+    let password = document.getElementById('password').value;
+    let email = document.getElementById('email').value;
+
+    // first name 
+    try {
+      let result = checkName(firstname, 'First Name');
+      let ferror = document.getElementById('ferror');
+      ferror.classList.add('hidden');
+    } catch (e) {
+      event.preventDefault();
+      let ferror = document.getElementById('ferror');
+      ferror.textContent = e;
+      //ferror.classList.remove('hidden');
+      //error.classList.add('hidden');
+    }
+    // last name 
+    try {
+      let result = checkName(lastname, 'Last Name');
+      let lerror = document.getElementById('lerror');
+      lerror.classList.add('hidden');
+    } catch (e) {
+      event.preventDefault();
+      let lerror = document.getElementById('lerror');
+      lerror.textContent = e;
+    }
+    // email
+    try {
+      let result = checkEmail(email, 'Email Address');
+      let eerror = document.getElementById('eerror');
+      eerror.classList.add('hidden');
+    } catch (e) {
+      event.preventDefault();
+      let eerror = document.getElementById('eerror');
+      eerror.textContent = e;
+    }
+
+    // age
+    try {
+      let result = checkAge(Number(age), 'Age');
+      let aerror = document.getElementById('aerror');
+      aerror.classList.add('hidden');
+    } catch (e) {
+      event.preventDefault();
+      let aerror = document.getElementById('aerror');
+      aerror.textContent = e;
+    }
+
+    // password
+    try {
+      let result = checkPasswordSignUp(password, 'Password');
+      let perror = document.getElementById('perror');
+      perror.classList.add('hidden');
+    } catch (e) {
+      event.preventDefault();
+      let perror = document.getElementById('perror');
+      perror.textContent = e;
+    }
+    //signup.reset(); // do i need this? 
+    // ferror.reset(); 
+  });
+
+  document.getElementById('signup').addEventListener('reset', function (event) {
+    //event.preventDefault();
+    let ferror = document.getElementById('ferror');
+    ferror.classList.add('hidden');
+    let perror = document.getElementById('perror');
+    perror.classList.add('hidden');
+    let lerror = document.getElementById('lerror');
+    lerror.classList.add('hidden');
+    let eerror = document.getElementById('eerror');
+    eerror.classList.add('hidden');
+    let aerror = document.getElementById('aerror');
+    aerror.classList.add('hidden');
+
+  });
+
+
+
   $('#signup-form').submit(function (event) {
     event.preventDefault();
 
@@ -68,34 +161,34 @@ const checkEmail = (email, varName) => {
     let password = $('#password').val();
 
     try {
-        checkName(firstname, 'First Name');
-        checkName(lastname, 'Last Name');
-        checkEmail(email, 'Email Address');
-        checkAge(Number(age), 'Age');
-        checkPasswordSignUp(password, 'Password');
+      checkName(firstname, 'First Name');
+      checkName(lastname, 'Last Name');
+      checkEmail(email, 'Email Address');
+      checkAge(Number(age), 'Age');
+      checkPasswordSignUp(password, 'Password');
 
-        
-        $.ajax({
-            method: 'POST',
-            url: '/users/signup',
-            contentType: 'application/json',
-            data: JSON.stringify({
-                firstName: firstname,
-                lastName: lastname,
-                age: age,
-                email: email,
-                password: password
-            })
-        }).done(function (response) {
-            
-            window.location.href = '/users/profile';
-        }).fail(function (error) {
-             
-            $('#signup-error').text("Sign up failed: " + error.responseJSON.message);
-        });
+
+      $.ajax({
+        method: 'POST',
+        url: '/users/signup',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          firstName: firstname,
+          lastName: lastname,
+          age: age,
+          email: email,
+          password: password
+        })
+      }).done(function (response) {
+
+        window.location.href = '/users/profile';
+      }).fail(function (error) {
+
+        $('#signup-error').text("Sign up failed: " + error.responseJSON.message);
+      });
     } catch (error) {
-        
-        $('#signup-error').text(error);
+
+      $('#signup-error').text(error);
     }
-    });
-  })(window.jQuery);
+  });
+})(window.jQuery);
