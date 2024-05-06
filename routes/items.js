090a4786a11a2b5e4d0a449d4fa73bc894144788
Lilli Nappi <116825146+nappilil@users.noreply.github.com>
2024-05-06 10:36:48 -0400

    minor fix to update items

--- routes/items.js
+++ routes/items.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 import { groceryItemsData } from '../data/index.js';
-import { checkString, checkAge } from '../validation.js';
+import { checkString, checkAge, checkId } from '../validation.js';
 import xss from 'xss';
 
 router.route('/createItem')
