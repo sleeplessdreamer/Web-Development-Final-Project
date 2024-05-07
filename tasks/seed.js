c27163653abf2a099bde34d2fe1da8717bd1a95f
Lilli Nappi <116825146+nappilil@users.noreply.github.com>
2024-05-06 21:53:58 -0400

    update seed
    has more info to database to populate

--- tasks/seed.js
+++ tasks/seed.js
@@ -4,6 +4,7 @@ import households from '../data/household.js';
 import announcements from '../data/announcementPost.js';
 import items from '../data/groceryListItems.js';
 import lists from '../data/groceryList.js';
+import comments from '../data/comments.js';
 
 const db = await dbConnection();
 await db.dropDatabase();
@@ -44,7 +45,7 @@ userTwo = await users.addUser(
 );
 
 let userThree;
-// Add another user with same name
+// Add another user with same name with append number to their name
 userThree = await users.addUser(
     "adeshmukh@gmail.com",
     "cannotB3h@cked!",
@@ -101,7 +102,7 @@ let login = await users.logInUser(
     "Password123!"
 )
 //console.log(login);
-
+// invalid login
 try {
     await users.logInUser(
         "adeshmukh@stevens.edu",
@@ -110,7 +111,7 @@ try {
 } catch (e) {
     //console.log(e);
 }
-
+// invalid login
 try {
     await users.logInUser(
         "lnappi@stevens.edu",
@@ -119,17 +120,17 @@ try {
 } catch (e) {
     //console.log(e);
 }
-
+// valid create house
 let house = await households.createHousehold(
     "Nappi",
     userOne._id.toString()
 );
-
+// valid join household
 await households.joinHousehold(
     "Nappi",
     dad._id.toString()
 )
-
+// valid join household
 await households.joinHousehold(
     "Nappi",
     mom._id.toString()
@@ -140,7 +141,7 @@ ourHousehold = await households.createHousehold(
     "GirlBosses",
     userTwo._id.toString()
 );
-
+// create household that already exists (invalid)
 try {
     await households.createHousehold(
         "GirlBosses",
@@ -149,46 +150,51 @@ try {
 } catch (e) {
     //console.log(e);
 }
-
+// join household that does not exist
 try {
     await households.joinHousehold(
-        "girlboss",
+        "Girlboss",
         userThree._id.toString()
     )
 } catch (e) {
     //console.log(e);
 }
-
+// join household valid
 ourHousehold = await households.joinHousehold(
     "GirlBosses",
     userThree._id.toString()
 );
 //console.log(ourHousehold);
 
+// join household valid
 ourHousehold = await households.joinHousehold(
     "GirlBosses",
     userFour._id.toString()
 );
 
+// join household valid
 ourHousehold = await households.joinHousehold(
     "GirlBosses",
     userFive._id.toString()
 );
 
+// join household valid
 ourHousehold = await households.joinHousehold(
     "GirlBosses",
     userSix._id.toString()
 );
 
+// get all households
 await households.getAllHouseholds();
 
+// get all announcements by household name
 let announcement = await announcements.getAllAnnouncementsByHouseholdName(ourHousehold.householdName);
 //console.log(announcement);
 
 
-//attempting to make a grocery list
 let listOne;
 //console.log(userTwo._id);
+// make a grocery list valid
 try {
     listOne = await lists.newGroceryList(
         userTwo._id,
@@ -201,6 +207,7 @@ try {
 }
 //console.log(listOne); // before
 let newitem;
+// add an item to grocery list (valid)
 try {
     newitem = await items.newItem(
         userTwo._id.toString(),
@@ -215,7 +222,8 @@ try {
     console.log(e);
 }
 //console.log(listOne); // after item is after
-try {//testing to see if creating a new item with the same name will update the item properly instead of creating a new item
+// adds item that already exists to a list will increase quantity instead of adding new item
+try {
     let dupeItem = await items.newItem(
         userFive._id.toString(),
         listOne._id.toString(),
@@ -228,6 +236,7 @@ try {//testing to see if creating a new item with the same name will update the 
 } catch (e) {
     console.log(e);
 }
+// new item (valid)
 try {
     await items.newItem(
         userFour._id.toString(),
@@ -241,6 +250,7 @@ try {
 } catch (e) {
     console.log(e);
 }
+// new item (valid)
 try {
     await items.newItem(
         userTwo._id.toString(),
@@ -254,12 +264,14 @@ try {
 } catch (e) {
     console.log(e);
 }
+// get all items by list (valid)
 try {
     await items.getAllItems(listOne._id.toString());
 } catch (e) {
     console.log(e);
 }
 let item;
+// get item by id
 try {
     item = await items.getItemById(newitem.items[0]._id.toString());
 } catch (e) {
@@ -267,6 +279,7 @@ try {
 }
 
 let update;
+// update an item in a list (valid)
 try {
     update = await items.updateItem(item._id.toString(),
         {
@@ -281,6 +294,7 @@ try {
 }
 //console.log(update);
 let thingy;
+// get grocery list by id (valid)
 try {
     thingy = await lists.getGroceryList(listOne._id.toString());
     //console.log(thingy);
@@ -288,13 +302,14 @@ try {
     console.log(e);
 }
 let del2
+// delete item in a grocery list (valid)
 try {
     del2 = await items.deleteLItem(listOne._id.toString(), item._id.toString(), userFive._id);
     //console.log(del2);
 } catch (e) {
     console.log(e);
 }
-// putting it back
+// putting item back (valid)
 try {
     await items.newItem(
         userSix._id.toString(),
@@ -307,9 +322,10 @@ try {
 } catch (e) {
     console.log(e);
 }
-
+// creating another list (valid)
+let list2;
 try {
-    await lists.newGroceryList(
+    list2 = await lists.newGroceryList(
         userThree._id,
         "GirlBosses",
         "Stuff I Need",
@@ -318,7 +334,28 @@ try {
 } catch (e) {
     console.log(e)
 }
+let item3;
+try {
+    item3 = await items.newItem(
+        userSix._id.toString(),
+        list2._id.toString(),
+        "Apples",
+        3,
+        "Low",
+        "Fruits"
+    );
+} catch (e) {
+    console.log(e);
+}
 
+await comments.newComment(
+    userSix._id.toString(),
+    list2._id.toString(),
+    item3.items[0]._id.toString(),
+    "I need more apples!"
+)
+
+// creating another list (valid)
 try {
     await lists.newGroceryList(
         userFour._id,
@@ -329,7 +366,7 @@ try {
 } catch (e) {
     console.log(e)
 }
-// user has to belong to household to make grocery lists for it will throw
+// user has to belong to household to make grocery lists or it will throw
 try {
     await lists.newGroceryList(
         userOne._id,
@@ -340,7 +377,7 @@ try {
 } catch (e) {
     //console.log(e)
 }
-
+// make new list (valid)
 try {
     await lists.newGroceryList(
         userFour._id,
@@ -351,13 +388,14 @@ try {
 } catch (e) {
     console.log(e)
 }
-
+// get all lists by household name (valid)
 try {
     await households.getAllGroceryListsByHousehold("Girlbosses");
 } catch (e) {
     console.log(e);
 }
 let newList = undefined;
+// make new list (valid)
 try {
     newList = await lists.newGroceryList(
         userFour._id,
@@ -368,6 +406,7 @@ try {
 } catch (e) {
     console.log(e)
 }
+// update the list (valid)
 try {
     newList = await lists.updateGroceryList(
         newList._id.toString(),
@@ -379,6 +418,7 @@ try {
     console.log(e)
 }
 let del = undefined;
+// delete the list (valid)
 try {
     del = await lists.deleteGroceryList(newList._id.toString(), "GirlBosses", userThree._id.toString());
 } catch (e) {
@@ -386,20 +426,146 @@ try {
 }
 //console.log(del);
 let a;
+// get user by id (valid)
 try {
     a = await users.getUserById(userFive._id);
 } catch (e) {
     console.log(e);
 }
 //console.log(a); // no comment for announcement
+// update announcement i.e. leave a comment on their announcement (this announcement is from when they join the household)
 try {
     a = await announcements.updateAnnouncement(a.announcements[0]._id.toString(), "Hey everyone!");
 } catch (e) {
     console.log(e);
 }
 //console.log(a); // comment for announcement
+// get all announcements by household name (valid)
 let announcement2 = await announcements.getAllAnnouncementsByHouseholdName(ourHousehold.householdName);
 //console.log(announcement2);
+
+// additional seeding
+// Add user (valid)
+// user forgot their password will keep adding number to their name
+let newUser;
+newUser = await users.addUser(
+    "kbloomer@stevens.edu",
+    "Password123!",
+    "Katie",
+    "BlooMEr",
+    21
+);
+// password cannot contain spaces
+await households.createHousehold(
+    "iHeartNoahKahan",
+    newUser._id.toString()
+);
+newUser = await users.addUser(
+    "kbloomer@gmail.com",
+    "Password123!",
+    "Katie",
+    "BlooMEr",
+    21
+);
+await households.joinHousehold(
+    "iHeartNoahKahan",
+    newUser._id.toString()
+);
+newUser = await users.addUser(
+    "kbloomer@yahoo.edu",
+    "Password123!",
+    "Katie",
+    "BlooMEr",
+    21
+);
+await households.joinHousehold(
+    "iHeartNoahKahan",
+    newUser._id.toString()
+);
+try {
+    listOne = await lists.newGroceryList(
+        newUser._id,
+        "iHeartNoahKahan",
+        "School Supplies",
+        "personal"
+    );
+} catch (e) {
+    console.log(e)
+}
+try {
+    newitem = await items.newItem(
+        newUser._id.toString(),
+        listOne._id.toString(),
+        "Pack of pens",
+        2,
+        "Low",
+        "School"
+    );
+    //console.log(newitem); // new item
+} catch (e) {
+    console.log(e);
+}
+try {
+    newitem = await items.newItem(
+        newUser._id.toString(),
+        listOne._id.toString(),
+        "HASS Book",
+        1,
+        "Medium",
+        "School"
+    );
+    //console.log(newitem); // new item
+} catch (e) {
+    console.log(e);
+}
+newUser = await users.addUser(
+    "kbloomer@outlook.com",
+    "Password123!",
+    "Katie",
+    "BlooMEr",
+    21
+);
+await households.joinHousehold(
+    "iHeartNoahKahan",
+    newUser._id.toString()
+);
+try {
+    listOne = await lists.newGroceryList(
+        newUser._id,
+        "iHeartNoahKahan",
+        "My Birthday",
+        "special occasion"
+    );
+} catch (e) {
+    console.log(e)
+}
+try {
+    newitem = await items.newItem(
+        newUser._id.toString(),
+        listOne._id.toString(),
+        "Cake",
+        1,
+        "High",
+        "Dessert"
+    );
+    //console.log(newitem); // new item
+} catch (e) {
+    console.log(e);
+}
+try {
+    newitem = await items.newItem(
+        newUser._id.toString(),
+        listOne._id.toString(),
+        "Noah Kahan Tickets",
+        2,
+        "High",
+        "Gift"
+    );
+    //console.log(newitem); // new item
+} catch (e) {
+    console.log(e);
+}
+
 console.log()
 
 
